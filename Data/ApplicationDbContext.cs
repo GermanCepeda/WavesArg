8f3536daeef9ceaddbcefd199f6bad7bb0294ed6
GermanCepeda <cepedagerman02@gmail.com>
2025-06-27 12:46:05 -0300

    Initial commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext() : base() { }
+
+        public static string ConnectionString { get; set; } //Cadena de conexión
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+            optionsBuilder.UseSqlServer(ConnectionString);
+        }
+
+
+        public DbSet<Usuarios> Usuarios { get; set; }
+        public DbSet<Roles> Roles { get; set; }
+
+
+    }
+}
